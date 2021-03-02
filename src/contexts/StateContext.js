3047bce324f95dae3bfd45570085a8b334a91f43
jimmy <jimmy.lin@stonybrook.edu>
2021-03-01 20:35:32 -0500

    Added popup and random jobs

--- src/contexts/StateContext.js
+++ src/contexts/StateContext.js
@@ -3,7 +3,11 @@ import React, { useState, createContext } from 'react';
 export const StateContext = createContext();
 
 export const StateProvider = (props) => {
-    const [stateFeature, setStateFeature] = useState(null);
+    const [stateFeature, setStateFeature] = useState({
+        feature:null,
+        jobs:null,
+        job:null
+    });
 
     return (
         <StateContext.Provider value={[stateFeature, setStateFeature]}>
--- src/pages/StateSelection.js
+++ src/pages/StateSelection.js
@@ -15,6 +15,7 @@ const StateSelection = () => {
             geojson = data;
         });
 
+    const [showModal, setShowModal] = useState(false);
     const [polygonLayer, setPolygonLayer] = useState(null);
     const [polygonData, setPolygonData] = useState(null);
     const [feature, setFeature] = useState(null);
@@ -45,11 +46,19 @@ const StateSelection = () => {
     }
 
     const popUpClicked = (e) => {
+        console.log("clicked")
         e.preventDefault();
+        setShowModal(false);
+        setStateFeature({
+            feature: null,
+            jobs: null,
+            job: null
+        })
         setPopUpCoords({ latitude: e.lngLat[1], longitude: e.lngLat[0], state: e.features[0].properties.name });
         if (e.features[0].properties.name === undefined || e.features[0].properties.name === "Toronto") {
             //setShowPopup(false)
             setFeature(null);
+
             document.getElementById('state-selection').value = '';
         } else {
             //setShowPopup(true);
@@ -75,8 +84,28 @@ const StateSelection = () => {
         console.log(cur_feature);
 
         setFeature(cur_feature);
-        setStateFeature(cur_feature);
+        let temp_feature = {
+            feature: cur_feature,
+            jobs: null,
+            job: null
+        }
+        setStateFeature(temp_feature);
         if (cur_feature) {
+
+            //10 jobs with random number of districtings
+            let jobs = [];
+            for (let i = 0; i < 10; i++) {
+                jobs.push(Math.floor(Math.random() * 50000));
+            }
+
+            let temp_feature = {
+                feature: cur_feature,
+                jobs: jobs,
+                job: stateFeature.job
+            };
+
+            setStateFeature(temp_feature);
+
             setPolygonData({
                 'type': cur_feature.type,
                 'geometry': {
@@ -106,10 +135,29 @@ const StateSelection = () => {
         getState(state);
     }
 
+    const jobClick = (e) => {
+        e.preventDefault();
+        setShowModal(true);
+        const index = parseInt(e.target.id.split("-")[1]);
+        const temp_feature = {
+            feature: stateFeature.feature,
+            jobs: stateFeature.jobs,
+            job: index - 1
+        }
+        setStateFeature(temp_feature);
+        console.log(stateFeature);
+        console.log(`Job ${index} has ${stateFeature.jobs[index - 1]} districtings.`)
+    }
+
+    const closePopup = (e) => {
+        e.preventDefault();
+        setShowModal(false);
+    }
+
     return (
         <div className="container-fluid" style={{ height: "100vh", width: "100vw", position: 'relative' }}>
             <div className="row d-flex justify-content-between" style={{ height: "100%", width: "100%", position: 'absolute', top: '0' }}>
-                <div id="left-bar" className="col-2" style={{ backgroundColor: "#fff", zIndex: "2" }}>
+                <div id="left-bar" className="col-2" align="center" style={{ backgroundColor: "#fff", zIndex: "2" }}>
                     <h3>State selection:</h3>
                     <select id="state-selection" onChange={stateSelection}>
                         <option value="" defaultValue hidden>Select a state</option>
@@ -164,11 +212,25 @@ const StateSelection = () => {
                         <option value="WI">Wisconsin</option>
                         <option value="WY">Wyoming</option>
                     </select>
+                    {stateFeature.jobs !== null ? (
+                        <div className="d-flex flex-column justify-content-between py-4" style={{ height: "80%" }}>
+                            {stateFeature.jobs.map((job, index) => {
+                                return (
+                                    <div key={index + 1}>
+                                        <button id={`job-${index + 1}`} className="btn btn-primary" onClick={jobClick}>Job {index + 1}</button>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    ) : ""}
+
+
+
                 </div>
 
-                <div id="right-bar" className="col-2" style={{ backgroundColor: "#fff", zIndex: "2" }}>
+                {/* <div id="right-bar" className="col-2" style={{ backgroundColor: "#fff", zIndex: "2" }}>
                     bar 2
-                </div>
+                </div> */}
             </div>
 
 
@@ -180,8 +242,8 @@ const StateSelection = () => {
                 mapboxApiAccessToken={"pk.eyJ1IjoieGxpdHRvYm95eHgiLCJhIjoiY2tscHFmejN4MG5veTJvbGhyZjFoMjR5MiJ9.XlWX6UhL_3qDIlHl0eUuiw"}
                 onClick={popUpClicked}
             >
-                {/* {console.log("State feature: ", stateFeature)} */}
-                {feature !== null ? (
+                {stateFeature.feature !== null ? console.log("feature is ", stateFeature) : ""}
+                {stateFeature.feature !== null ? (
                     <Source
                         id="state"
                         type="geojson"
@@ -216,6 +278,26 @@ const StateSelection = () => {
                 </Popup>
                 } */}
             </ReactMapGL>
+
+            {showModal ? (
+                <div class="card" style={{
+                    zIndex: "2",
+                    height: "20%",
+                    width: "20%",
+                    position: "absolute",
+                    backgroundColor: "rgba(255,255,255,0.8)",
+                    bottom: "5%",
+                    right: "50%",
+                    left: "50%"
+                }}>
+                    <div className="card-body">
+                        <h5 className="card-title">Job {stateFeature.job + 1}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{stateFeature.feature.properties.name}</h6>
+                        <p className="card-text">This job for {stateFeature.feature.properties.name} has {stateFeature.jobs[stateFeature.job]} districtings.</p>
+                        <button className="btn btn-primary" onClick={closePopup}>Close</button>
+                    </div>
+                </div>
+            ) : ""}
 
         </div>
 
